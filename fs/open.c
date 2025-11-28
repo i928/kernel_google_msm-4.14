f6288a2e2a7b7032eaa5cad864bc4a16fb90ac30
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-11-28 09:25:04 +0800

    fs/kernel: update manual hooks to v1.6

--- fs/open.c
+++ fs/open.c
@@ -355,7 +355,7 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 }
 
 #ifdef CONFIG_KSU
-__attribute__((hot))
+__attribute__((hot)) 
 extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
 				int *mode, int *flags);
 #endif
