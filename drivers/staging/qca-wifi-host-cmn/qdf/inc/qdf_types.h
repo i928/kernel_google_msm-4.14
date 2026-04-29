91a10b8df831a93113631ffcd19368df63a60673
abu-adm <github@toruk.cz>
2026-04-29 22:37:44 +0200

    qcacld: disable some debug and logging features
    - inspired by pascua28/android_kernel_samsung_sm7150 commit
      399307cdab24032e8c7c2764efda391e260304cb
      qcacld: disable debugging bloat as much as possible
    
      Change-Id: I0b914a70b4369c200e0c967a8b97ba4b39387f0b
      Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
      Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    
      (unfortunately, only fraction can be applied)
    
    - squashed with ea02fac8c49e0b3f0743d5dd69b3204f9bf2135f from same repo
      qcacmn: Properly resolve disabled diag procfs
    
      Change-Id: I02c7464ed70c560a5dcda2f5896ee6a815cb8b90
      Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/staging/qca-wifi-host-cmn/qdf/inc/qdf_types.h
+++ drivers/staging/qca-wifi-host-cmn/qdf/inc/qdf_types.h
@@ -573,6 +573,7 @@ enum QDF_GLOBAL_MODE {
 
 #define  QDF_IS_EPPING_ENABLED(mode) (mode == QDF_GLOBAL_EPPING_MODE)
 
+#ifdef QDF_ENABLE_TRACING
 /**
  * qdf_trace_msg()- logging API
  * @module: Module identifier. A member of the QDF_MODULE_ID enumeration that
@@ -594,6 +595,8 @@ enum QDF_GLOBAL_MODE {
  */
 void __printf(3, 4) qdf_trace_msg(QDF_MODULE_ID module, QDF_TRACE_LEVEL level,
 		   char *str_format, ...);
+#endif /* QDF_ENABLE_TRACING */
+
 /**
  * qdf_vtrace_msg() - the va_list version of qdf_trace_msg
  * @module: the calling module's Id
